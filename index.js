4b7d47c4437c6cfe22007dbd5f89d4aa2125b263
deuojen <qnurix@hotmail.com>
2024-10-24 22:56:46 +0100

    feat: complete rask

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
 
 var app = express();
 
@@ -11,10 +13,18 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+// /api/fileanalyse
 
-
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+  console.log(req.file, req.body);
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size,
+  });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log('Your app is listening on port ' + port);
 });
